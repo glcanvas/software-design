7d75a1acf67b4c76b4e386427a87479c30fa18c0
glcanvas <duginetsn98@gmail.com>
2019-10-07 09:49:30 +0300

    api client test impl

--- src/test/java/com/nduginets/softwaredesign/apiclient/ApiTest.java
+++ src/test/java/com/nduginets/softwaredesign/apiclient/ApiTest.java
@@ -1,11 +1,10 @@
 package com.nduginets.softwaredesign.apiclient;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.tomakehurst.wiremock.WireMockServer;
-import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,28 +16,48 @@ import java.util.concurrent.TimeUnit;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class ApiTest {
-    private static final Instant time = Instant.ofEpochSecond(10);
+    private static final Instant startTime = Instant.ofEpochSecond(10);
     private static final int hours = 4;
+    private static final Instant endTime = startTime.plusSeconds(TimeUnit.HOURS.toSeconds(4));
     private static final String query = "A";
+    private static final WireMockServer stubServer = new WireMockServer(8089);
 
     private final ObjectMapper mapper = new ObjectMapper();
-    private Api apiMock;
-    private Api apiStub;
 
-    @Rule
-    private final WireMockServer wireMockServer = new WireMockServer(8089);
+    private Api apiStub;
+    private Api apiMock;
+
+    @BeforeEach
+    public void initStubServer() {
+        String testUrl = String.format("/news?start_time=%d&end_time=%d&q=A&access_token=stub&v=5.101",
+                startTime.getEpochSecond(),
+                endTime.getEpochSecond());
+        stubServer.start();
+        stubServer.
+                stubFor(
+                        get(urlEqualTo(testUrl))
+                                .willReturn(aResponse()
+                                        .withBody(createRootNode())));
+        ApiClient clientStub = ApiClient.buildApiClient("http://localhost:8089/news", "stub");
+        apiStub = new Api(clientStub);
+    }
 
     @BeforeEach
     public void initMock() {
         ApiClient clientMock = Mockito.mock(ApiClient.class);
-        Mockito.when(clientMock.getNews(query, time, hours))
+        Mockito.when(clientMock.getNews(query, startTime, hours))
                 .thenReturn(createArrayNode());
         apiMock = new Api(clientMock);
     }
 
+    @AfterEach
+    public void stopStubServer() {
+        stubServer.stop();
+    }
+
     @Test
     public void mockTest() {
-        Histogram h = apiMock.createHistogram(query, time, hours);
+        Histogram h = apiMock.createHistogram(query, startTime, hours);
         for (int i = 1; i <= hours; i++) {
             Assertions.assertEquals(1, h.getValue(i));
         }
@@ -46,15 +65,7 @@ public class ApiTest {
 
     @Test
     public void stubTest() {
-        wireMockServer.start();
-        wireMockServer.
-                stubFor(
-                        get(urlEqualTo("/news?start_time=10&end_time=14410&q=A&access_token=stub&v=5.101"))
-                                .willReturn(aResponse()
-                                        .withBody(createRootNode())));
-        ApiClient clientStub = ApiClient.buildApiClient("http://localhost:8089/news", "stub");
-        apiStub = new Api(clientStub);
-        Histogram h = apiStub.createHistogram(query, time, hours);
+        Histogram h = apiStub.createHistogram(query, startTime, hours);
         for (int i = 1; i <= hours; i++) {
             Assertions.assertEquals(1, h.getValue(i));
         }
@@ -64,7 +75,7 @@ public class ApiTest {
         ArrayNode nodes = mapper.createArrayNode();
         for (int i = 1; i <= hours; i++) {
             ObjectNode node = mapper.createObjectNode();
-            node.put("date", time.getEpochSecond() + TimeUnit.HOURS.toSeconds(i));
+            node.put("date", startTime.getEpochSecond() + TimeUnit.HOURS.toSeconds(i));
             nodes.add(node);
         }
         return nodes;
